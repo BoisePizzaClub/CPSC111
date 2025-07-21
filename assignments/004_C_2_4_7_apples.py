0032185663101a1b1f70ce2b9e27f48431997d98
George <boisepizzaclub@gmail.com>
2025-07-21 01:40:56 -0600

    done

--- assignments/004_C_2_4_7_apples.py
+++ assignments/004_C_2_4_7_apples.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 #George Yang
 #003_C_2_4_7_apples.py
+
+john = 8
+mary = 11
+adam = 2
+
+print(john, mary, adam, sep=",")
+total_apples = john + mary + adam
+print(total_apples)
+
+#experiments:
+
+# print("john has", john ,"apples,", "mary has", mary, "apples,"," and Adan has", adam, "apples.")
+
+# print("The total number of apples is", total_apples,)
