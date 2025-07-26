6ab36711e25cf5aa7bd6416ad139156df418fb0d
George <boisepizzaclub@gmail.com>
2025-07-25 23:46:34 -0600

    :(

--- assignments/014_C_3_1_12_Lab_3_essentials_of_if_elif_else.py
+++ assignments/014_C_3_1_12_Lab_3_essentials_of_if_elif_else.py
@@ -3,9 +3,39 @@
 #014_C_3_1_12_Lab_3_essentials_of_if_elif_else.py
 
 year = int(input("Enter a year: "))
+type = ""
 
 if year < 1582:
 	print("Not within the Gregorian calendar period")
 else:
     #  Write the if-elif-elif-else block here.
-	
+	if year % 4 != 0:
+		print("The year", year, "is a common year.")
+	elif year % 100 != 0:
+		print("The year", year, "is a leap year.")
+	elif year % 400 != 0:
+		print("The year", year, "is a common year.")
+	else:
+		print("The year", year, "is a leap year.")
+		
+#print("The year", year, "is a common year.")
+#print("The year", year, "is a leap year.")
+
+#experiment failed, wrong logic and prints excess line..
+
+# year = int(input("Enter a year: "))
+# type = ""
+
+# if year < 1582:
+# 	print("Not within the Gregorian calendar period")
+# else:
+#     #  Write the if-elif-elif-else block here.
+# 	if year % 4 != 0:
+# 		type = "common"
+# 	elif year % 100 != 0:
+# 		type = "leap"
+# 	elif year % 400 != 0:
+# 		type = "common"
+# 	else:
+# 		type = "leap"
+# print("The year", year, "is a", type, "year.")
--- assignments/015_C_3_2_4_Lab_1_guess_the_number.py
+++ assignments/015_C_3_2_4_Lab_1_guess_the_number.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 #George Yang
 #015_C_3_2_4_Lab_1_guess_the_number.py
+
+
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
